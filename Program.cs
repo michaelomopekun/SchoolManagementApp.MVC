cc229d6f5b5da4133186bfceb17732e93390a42a
omopekun michael <omopekunmichael@gmail.com>
2025-02-05 08:04:41 +0100

    Modified web views and layout

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using Auth0.AspNetCore.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = builder.Configuration["Auth0:Domain"];
+    options.ClientId = builder.Configuration["Auth0:ClientId"];
+});
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -16,6 +26,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
